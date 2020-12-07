4f1d972ae7f89a850bc5eb49eb3533b11f4ef425
calc-inv <75644444+calc-inv@users.noreply.github.com>
2020-12-08 01:24:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 function monthly_profit(){
-		let chislo = Number(document.getElementById("chislo").value);
-		let procent = Number(document.getElementById("procent").value);
+		let deposit = Number(document.getElementById("deposit").value);
+		let percent = Number(document.getElementById("percent").value);
 		let profit = 0;
-		profit = chislo / 100 * procent;
+		profit = deposit / 100 * percent;
 		document.getElementById("proc").value = Math.round(profit);
 	}
 
 function func(){
 	document.getElementById("year").innerHTML = "";
 	document.getElementById("res").innerHTML = "";
-	let chislo = Number(document.getElementById("chislo").value);
+	let deposit = Number(document.getElementById("deposit").value);
 	let count = Number(document.getElementById("count").value);
-	let procent = Number(document.getElementById("procent").value);
+	let percent = Number(document.getElementById("percent").value);
 	let tax = Number(document.getElementById("tax").value);
-	let year = 2020;
+	var date = new Date();
+	let year = date.getFullYear();
 	if(document.getElementById("yes").checked){
 	tax = tax/100;
 		for(let i = 0; i < count; i++){
-			chislo = parseInt(chislo) + ((chislo / 100) * procent) - chislo * tax;
+			deposit = parseInt(deposit) + ((deposit / 100) * percent) - deposit * tax;
 			year += 1;
 			document.getElementById("year").innerHTML += year + "<br>";
-			document.getElementById("res").innerHTML += chislo.toFixed(2) + " ₽" + "<br>";
+			document.getElementById("res").innerHTML += deposit.toFixed(2) + " ₽" + "<br>";
 		}
 	}
 	else if(document.getElementById("no").checked){
 	  	for(let i = 0; i < count; i++){
-			chislo = parseInt(chislo) + ((chislo / 100) * procent);
+			deposit = parseInt(deposit) + ((deposit / 100) * percent);
 			year += 1;
 			document.getElementById("year").innerHTML += year + "<br>";
-			document.getElementById("res").innerHTML += chislo.toFixed(2) + " ₽" + "<br>";
+			document.getElementById("res").innerHTML += deposit.toFixed(2) + " ₽" + "<br>";
 		}
 	}
 }
